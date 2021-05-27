ea71316a278133f2ad084c7c82d0eba89a3d476f
azhari <azharisaputrasikumbang@gmail.com>
2021-05-27 18:37:29 +0700

    add: authentication scaffolding (breeze)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,3 @@
-window.Vue = require('vue').default;
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-const app = new Vue({
-	el: '#app'
-})
-
 require('./bootstrap');
+
+require('alpinejs');
