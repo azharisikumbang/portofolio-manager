848950bc1f9514a650dd70e18d345426c33e7db9
azhari <azharisaputrasikumbang@gmail.com>
2021-05-27 17:53:37 +0700

    add: frontend stack modules

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
+window.Vue = require('vue').default;
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+	el: '#app'
+})
+
 require('./bootstrap');
