320a6c17ff9a80d689a2615007ea4c9c1b526d0c
azhari <azharisaputrasikumbang@gmail.com>
2021-06-03 20:29:12 +0700

    add: education factory

---
+++ database/factories/EducationFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Education;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EducationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Education::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'institution' => $this->faker->company(),
+            'degree' => $this->faker->word(),
+            'start_period' => $this->faker->date(),
+            'end_period' => $this->faker->date()
+        ];
+    }
+}
