9d6303db64a1ab193ad181978c1a5014fba7ebce
azhari <azharisaputrasikumbang@gmail.com>
2021-06-14 00:48:25 +0700

    add global test helper

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,45 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+   	protected function getActor()
+   	{
+   		return User::factory()->create();
+   	}
+
+   	protected function userSigningIn()
+   	{
+   		$user = $this->getActor();
+   		$this->actingAs($user);
+   		
+   		return $user; 
+   	}
+
+      protected function getDataFromFactory(string $model, array $modifier = [], int $count = 1)
+      {
+         if (!class_exists($model)) {
+            throw new ModelNotFoundException("Failed to create sample Data, model class $model doesn't exists.", 1);
+         }
+
+         if (!method_exists($model, 'factory')) {
+            throw new Exception("{$model} does not implement factory.", 1);
+         }
+
+         $results = $model::factory()->count($count)->make()->toArray();
+
+         if ($modifier) {
+            foreach ($results as $key => $result) {
+               $results[$key] = array_merge($result, $modifier);
+            }
+         }
+
+         return $count > 1 ? $results : $results[0];
+      }
 }
